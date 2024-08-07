5f89c94d8a0eb0df3d133671080c7eeaa065b76b
pankajnelsontirkey <pankajnelsontirkey@outlook.com>
2024-08-07 23:05:55 +0530

    done maps application

---
+++ maps/src/CustomMap.ts
@@ -0,0 +1,42 @@
+/// <reference types="@types/google.maps" />
+
+export interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+  markerContent(): string;
+}
+
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor(containerId: string) {
+    this.googleMap = new google.maps.Map(
+      document.getElementById(containerId) as HTMLElement,
+      {
+        zoom: 1,
+        center: { lat: 0, lng: 0 },
+        mapId: google.maps.Map.DEMO_MAP_ID
+      }
+    );
+  }
+
+  addMarker(mappable: Mappable): void {
+    const marker = new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng
+      }
+    });
+
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: mappable.markerContent()
+      });
+
+      infoWindow.open(this.googleMap, marker);
+    });
+  }
+}

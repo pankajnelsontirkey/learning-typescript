d8a5928900b635df48ab8daffcead7039c2bf064
pankajnelsontirkey <pankajnelsontirkey@outlook.com>
2025-02-15 02:13:58 +0530

    simple analysis on data; bad version

---
+++ stats/src/index.ts
@@ -0,0 +1,21 @@
+import fs from 'fs';
+
+const matches = fs
+  .readFileSync('football.csv', { encoding: 'utf-8' })
+  .split('\n')
+  .map((row: string): string[] => row.split(','));
+
+// console.log('matches', matches);
+
+let manUnitedWins = 0;
+
+for (let match of matches) {
+  if (
+    (match[1] === 'Man United' && match[5] === 'H') ||
+    (match[2] === 'Man United' && match[5] === 'A')
+  ) {
+    manUnitedWins += 1;
+  }
+}
+
+console.log('manUnitedWins', manUnitedWins);

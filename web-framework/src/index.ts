b734b191ec48d7ed056db267e3100b1c963c4161
pankajnelsontirkey <pankajnelsontirkey@outlook.com>
2025-02-25 16:46:33 +0530

    added setter method for User class

--- web-framework/src/index.ts
+++ web-framework/src/index.ts
@@ -4,3 +4,8 @@ const user = new User({ name: 'Pankaj', age: 34 });
 
 console.log('Name: ', user.get('name'));
 console.log('Age: ', user.get('age'));
+
+user.set({ name: 'Pankaj Nelson Tirkey' });
+
+console.log('Name: ', user.get('name'));
+console.log('Age: ', user.get('age'));

b3e687474f39789b7c85db90fe36cf2c11451acb
pankajnelsontirkey <pankajnelsontirkey@outlook.com>
2025-02-28 01:14:29 +0530

    refactored code, created Model class as parent to User class

--- web-framework/src/index.ts
+++ web-framework/src/index.ts
@@ -1,21 +1,11 @@
 import { User } from './models/User';
 
-// const user = new User({ name: 'Pankaj Nelson Tirkey', age: 35 });
-// console.log(user['data']);
-// user.save();
+const user = User.buildUser({ id: 1 });
 
-const user = new User({ id: 1 });
+user.on('change', () => {
+  console.log('change!');
+});
+
 user.fetch();
-console.log(user['data']);
 
-// user.set({ name: 'Pankaj Nelson Tirkey', age: 35 });
-// user.save();
-// console.log(user['data']);
-
-// const user2 = new User({ name: 'Vineeta Minj', age: 33 });
-// console.log(user2['data']);
-// user2.save();
-
-const user2 = new User({ id: 2 });
-user2.fetch();
-console.log(user2['data']);
+console.log('user', user.getAll);

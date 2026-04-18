e8f39a1c59bb429ff6fdaf8a6c8d6a1b4ea48444
pankajnelsontirkey <pankajnelsontirkey@outlook.com>
2026-04-18 14:53:37 +0530

    prep for express;

--- features/metadata.ts
+++ features/metadata.ts
@@ -22,26 +22,35 @@
 
 import 'reflect-metadata';
 
-@printMetadata
+// @printMetadata
+@controller
 class Plane {
   color: string = 'red';
 
-  @markFunction('The launch codes!')
+  // @markFunction('The launch codes!')
+  @get('/login')
   fly(): void {
     console.log('shooom!');
   }
 }
 
-function markFunction(secretInfo: string) {
+// function markFunction(secretInfo: string) {
+function get(path: string) {
   return function (target: Plane, key: string) {
-    Reflect.defineMetadata('secret', secretInfo, target, key);
+    // Reflect.defineMetadata('secret', secretInfo, target, key);
+    Reflect.defineMetadata('path', path, target, key);
   };
 }
 
-function printMetadata(target: typeof Plane) {
+// function printMetadata(target: typeof Plane) {
+function controller(target: typeof Plane) {
   for (const key of Object.getOwnPropertyNames(target.prototype)) {
     if (key === 'constructor') continue;
-    const secret = Reflect.getMetadata('secret', target.prototype, key);
-    console.log('secret', secret);
+    // const secret = Reflect.getMetadata('secret', target.prototype, key);
+    // console.log('secret', secret);
+    const path = Reflect.getMetadata('path', target.prototype, key);
+    console.log('path', path);
+
+    /* router.get(path, target.prototype[key]) */
   }
 }

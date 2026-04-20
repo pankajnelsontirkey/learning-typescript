44ea69dac8c5e8ee2dc4c1c95bb037f299452aa3
pankajnelsontirkey <pankajnelsontirkey@outlook.com>
2026-04-20 23:25:01 +0530

    created RootController; added logout to LoginController;

--- node-express/src/index.ts
+++ node-express/src/index.ts
@@ -2,15 +2,14 @@ import cookieSession from 'cookie-session';
 import express from 'express';
 
 import { AppRouter } from './AppRouter';
+import './controllers/RootController';
 import './controllers/LoginController';
-import { router } from './routes/loginRoutes';
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ['somesecret'] }));
 
-app.use(router);
 app.use(AppRouter.getInstance());
 
 app.listen(3000, () => {

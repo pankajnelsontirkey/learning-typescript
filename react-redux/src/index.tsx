15aa21b50e1cbbc65bb1b0de36376f581c774b52
pankajnelsontirkey <pankajnelsontirkey@outlook.com>
2026-04-21 14:02:25 +0530

    added redux store; created counter reducer; created fetchTodos action with interfaces;

--- react-redux/src/index.tsx
+++ react-redux/src/index.tsx
@@ -1,5 +1,10 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
+
+import { App } from './components/App';
+import { reducers } from './reducers';
 
 interface AppProps {
   color?: string;
@@ -13,34 +18,11 @@ interface StateProps {
 //   return <div>{props.color}</div>;
 // };
 
-class App extends React.Component<AppProps, StateProps> {
-  // state = { counter: 0 };
+const store = createStore(reducers, applyMiddleware(thunk));
 
-  constructor(props: AppProps) {
-    super(props);
-
-    this.state = { counter: 0 };
-  }
-
-  onIncrement = (): void => {
-    this.setState({ counter: this.state.counter + 1 });
-  };
-
-  onDecrement = (): void => {
-    this.setState({ counter: this.state.counter - 1 });
-  };
-
-  render() {
-    // return <div>Hi there</div>;
-    // return <div>{this.props.color}</div>;
-    return (
-      <div>
-        <button onClick={this.onIncrement}>Increment</button>
-        <button onClick={this.onDecrement}>Decrement</button>
-        {this.state.counter}
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(<App color='red' />, document.querySelector('#root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
+);

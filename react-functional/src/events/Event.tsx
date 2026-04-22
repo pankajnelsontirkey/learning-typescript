964dc536e36188a81d59437c6877aca7d1a14e53
pankajnelsontirkey <pankajnelsontirkey@outlook.com>
2026-04-22 14:52:04 +0530

    annotate other event handlers;

--- react-functional/src/events/Event.tsx
+++ react-functional/src/events/Event.tsx
@@ -1,13 +1,20 @@
-import type { ChangeEvent, FC } from 'react';
+import type { ChangeEvent, DragEvent, FC } from 'react';
 
 const Event: FC = () => {
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     console.log(event.target.value);
   };
 
+  const handleDragStart = (event: DragEvent<HTMLDivElement>) => {
+    console.log('i am being dragged', event);
+  };
+
   return (
     <div>
       <input onChange={handleChange} />
+      <div draggable onDragStart={handleDragStart}>
+        Drag Me!
+      </div>
     </div>
   );
 };
